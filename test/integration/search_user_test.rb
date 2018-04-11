468176de217ebf8b52564fcb324980c6f684314a
kyo21south <kyotoge2186@yahoo.co.jp>
2018-04-11 18:04:22 +0900

    complete user search with ajax

--- test/integration/search_user_test.rb
+++ test/integration/search_user_test.rb
@@ -6,20 +6,23 @@ class SearchUserTest < ActionDispatch::IntegrationTest
     @michael = users(:michael)
     @archer = users(:archer)
     @malory = users(:malory)
+    log_in_as @michael
   end
 
   test "proper user should be appear" do
     # unactivated のユーザーが表示されないようにする
-    post users_path, params: { index: { query: "un"} }
-    follow_redirect!
-    assert_select 'a[href=?]', user_path(@unacti), text: @unacti.name, count: 0
+    post users_path, xhr: true, params: { search: { query: "un"} }
+    assert_no_match @unacti.name, response.body
+    # assert_select "a[href=?]", user_path(@unacti), text: @unacti.name, count: 0
     # あいまい検索が成功しているようにする
     get users_path
-    post users_path, params: { index: { query: "archer"} }
-    follow_redirect!
-    assert_select 'a[href=?]', user_path(@archer), text: @archer.name, count: 1
-    assert_select 'a[href=?]', user_path(@malory), text: @malory.name, count: 1
-    assert_select 'a[href=?]', user_path(@michael), text: @michael.name, count: 0
+    post users_path, xhr: true, params: { search: { query: "archer"} }
+    assert_match 'Archer', response.body
+    assert_match @malory.name, response.body
+    assert_no_match @michael.name, response.body
+    # assert_select "a[href=?]", user_path(@archer), text: @archer.name, count: 1
+    # assert_select "a[href=?]", user_path(@malory), text: @malory.name, count: 1
+    # assert_select "a[href=?]", user_path(@michael), text: @michael.name, count: 0
   end
 
 end
